565eb989bb55d62a973f5a27d8c89410fa27e0e3
Michael Womer <mwomer27@gmail.com>
2026-02-03 16:38:33 -0500

    added Hello World! to App

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript.  This is a message from Michael
             </header>
             <p>
+                Hello World!
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
