4f994b40e81d06fdd05850e34e378d19c6307e40
Michael <mwomer27@gmail.com>
2026-02-10 18:33:26 -0500

    made HTML and styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./App.css";
+import {Button, Container, Row, Col} from 'react-bootstrap';
+import forestImg from './assets/forest.jpg'
+
 
 function App(): React.JSX.Element {
     return (
@@ -7,11 +10,37 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript.  This is a message from Michael
             </header>
-            <p>
-                Hello World!
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+
+
+            <h1 style={ {backgroundColor:"green"}}>Header 1</h1>
+            <Container>
+            <Row>
+            <img src={forestImg} alt="An illustration of a village in a forest" style={ {height:'500px'}}></img>
+            <Col>
+                <p>My top three favorite colors:</p>
+                <ol>
+                    <li>Purple</li>
+                    <li>Red</li>
+                    <li>Green</li>
+                </ol>
+                <div style={{width:"100px", height:"50px", backgroundColor:"red"}}></div>
+            </Col>
+            <Col>
+                <Button onClick={()=>{console.log("Hello World!")}}>Log Hello World</Button>
+                <div style={{width:"100px", height:"50px", backgroundColor:"red"}}></div>
+
+            </Col>
+            <Col>
+                <p> 
+                    Hello World!
+                    Edit <code>src/App.tsx</code> and save. This page will
+                    automatically reload.
+                </p>
+                <div style={{width:"100px", height:"50px", backgroundColor:"red"}}></div>
+
+            </Col>
+            </Row>
+            </Container>
         </div>
     );
 }
