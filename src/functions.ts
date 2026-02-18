d358ee8946ee5e8f244fd20245ec7cf3d33b4116
Michael <mwomer27@gmail.com>
2026-02-17 23:26:34 -0500

    finished farenheitToCelius and add3 functions

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * 5/9;
 }
 
 /**
@@ -12,7 +12,13 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum:number= 0;
+    sum += first > 0 ? first : 0;
+    sum += second > 0 ? second : 0;
+    sum += third > 0 ? third : 0;
+
+
+    return sum;
 }
 
 /**
