ddd432827d8cecccf5d6f2191e7a2e01a472657a
Michael <mwomer27@gmail.com>
2026-02-23 18:39:46 -0500

    wrote publishQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -82,7 +82,8 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const oldPublished:boolean = question.published;
+    return {... question, published:!oldPublished};
 }
 
 /**
