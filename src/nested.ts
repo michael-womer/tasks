96062c8c9b06ea5d206702e6db1833ca5c9384ed
Michael <mwomer27@gmail.com>
2026-02-25 21:02:14 -0500

    wrote makeAnswers method

--- src/nested.ts
+++ src/nested.ts
@@ -98,7 +98,7 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    return questions.map((question:Question):Answer => {return {questionId: question.id, text:"", submitted:false, correct:false}});
 }
 
 /***
